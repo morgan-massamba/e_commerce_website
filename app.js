2e4254e9c28063a1cbed6af0e8a9c6eab7b93a9c
Morgan Massamba <chabcontact@gmail.com>
2020-07-22 16:50:38 +0200

    fonction translate cart

--- app.js
+++ app.js
@@ -76,20 +76,70 @@ class UI {
         event.target.innerText = 'In Cart';
         event.target.disabled = true;
         // get product from products
+        let cartItem = {...Storage.getProduct(id), amount : 1};
         // add product to the cart
+        cart = [...cart, cartItem]
         // save cart in local storage
+        Storage.saveCart(cart);
         // set cart values
+        this.setCartValues(cart);
         // display cart item
+        this.addCartItem(cartItem);
         // show the cart
+        this.showCart();
         })
     });
   }
+
+  setCartValues(cart){
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item =>{
+      itemsTotal += item.amount;
+      tempTotal += item.price * item.amount;
+    })
+    cartItems.innerText = itemsTotal;
+    cartTotal.innerText = parseFloat(tempTotal).toFixed(2);
+  }
+
+  addCartItem(item){
+    let div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = 
+    `
+        <img src=${item.image} alt="product">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>${item.price}$</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+        </div>
+        <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount">${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+        </div>
+    `;
+    cartContent.appendChild(div);
+  }
+
+  showCart(){
+    cartOverlay.classList.add('transparentBcg');
+    cartDOM.classList.add('showCart');
+  }
 }
+
 
 //local storage
 class Storage{
   static saveProducts(products){
     localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id){
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id === id);
+  }
+  static saveCart(cart){
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 

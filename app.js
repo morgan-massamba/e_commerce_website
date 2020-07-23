7b574645547601f20498b0a328ab456a7f45ce87
Morgan Massamba <chabcontact@gmail.com>
2020-07-23 10:20:10 +0200

    ajout fonctionalité

--- app.js
+++ app.js
@@ -126,6 +126,22 @@ class UI {
     cartOverlay.classList.add('transparentBcg');
     cartDOM.classList.add('showCart');
   }
+  hideCart(){
+    cartOverlay.classList.remove('transparentBcg');
+    cartDOM.classList.remove('showCart');
+  }
+
+  setupAPP(){
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener('click', this.showCart);
+    closeCartBtn.addEventListener('click', this.hideCart);
+  }
+
+  populateCart(cart){
+    cart.forEach(item => this.addCartItem(item));
+  }
 }
 
 
@@ -141,17 +157,26 @@ class Storage{
   static saveCart(cart){
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+  static getCart(){
+    return localStorage.getItem("cart")? JSON.parse(localStorage.getItem("cart")) : [];
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
 
+    //setup app
+    ui.setupAPP();
+
     //get all products
-    products.getProducts().then(products =>{
+    products
+    .getProducts()
+    .then(products =>{
       ui.displayProducts(products);
       Storage.saveProducts(products);
-    }).then( () => {
+      })
+    .then( () => {
       ui.getBagButtons();
     });
 });
